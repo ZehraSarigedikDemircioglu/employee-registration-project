fb94464838ef8c9ed2d9890c86591a215cf60438
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-07 15:58:51 -0500

    employee controller edited

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -6,10 +6,13 @@ import com.cydeo.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -28,7 +31,12 @@ public class EmployeeController {
     }
 
     @PostMapping("/insert")
-    public String insertEmployee(@ModelAttribute("employee") Employee employee) {
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("stateList", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
         employeeService.saveEmployee(employee);
         return "redirect:/employee/list"; // With redirect we are using endpoints
     }
