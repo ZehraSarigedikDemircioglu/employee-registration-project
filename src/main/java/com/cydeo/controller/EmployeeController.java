d124fe32851858a62ce345d06f249c0d3bbdfbe6
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-07 14:09:47 -0500

    employee controller edited

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,14 +1,22 @@
 package com.cydeo.controller;
+
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
+@AllArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
 
     @GetMapping("/register")
     public String createEmployee(Model model) {
@@ -17,6 +25,12 @@ public class EmployeeController {
         model.addAttribute("stateList", DataGenerator.getAllStates());
 
         return "employee/employee-create";
+    }
 
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "employee/employee-list";
+       // return "redirect:/employee/register";
     }
 }
