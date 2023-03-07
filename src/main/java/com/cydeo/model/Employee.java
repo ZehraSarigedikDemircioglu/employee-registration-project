28f52e5329c8770297cf1ab4ee39e19df52e10cc
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-07 14:08:50 -0500

    employee added

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -14,7 +14,7 @@ public class Employee {
     private String firstName;
     private String lastName;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd") // Thymeleaf accepts yyyy-MM-dd
     private LocalDate birthday;
     private String email;
     private String password;
